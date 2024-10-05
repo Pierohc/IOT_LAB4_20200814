153b4a0bed0a038464e810738ace4ad1e4bd4a18
Pierohc <pieritohc@hotmail.com>
2024-10-04 19:38:51 -0500

    aa

--- app/src/main/java/com/example/tele_futbol/MainActivity.java
+++ app/src/main/java/com/example/tele_futbol/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.tele_futbol;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public boolean isConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+
+    public void checkInternetConnection() {
+        if (!isConnected()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("No hay conexión a internet. ¿Quieres ir a los ajustes?")
+                    .setCancelable(false)
+                    .setPositiveButton("Configuración", (dialog, id) -> {
+                        startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+                    })
+                    .setNegativeButton("Cancelar", (dialog, id) -> dialog.cancel());
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +51,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        checkInternetConnection();
+
+
+        Button ingresar = findViewById(R.id.ingresar);
+
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AppActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
